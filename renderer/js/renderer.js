1c2bd09dd9c40ae5ae87ede85e7a3647f808ca70
Alex Hernandez Perez <anh010@ucsd.edu>
2023-11-13 18:00:29 -0800

    loaded file name and dimensions

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -13,7 +13,22 @@ function loadImage(e){
         console.log("Please use image file type.");
         return;
     }
-    console.log('success');
+    // get original dimension
+    const image =  new Image();
+    // create file object 
+    image.src = URL.createObjectURL(file);
+    // get dimen info
+    image.onload= function () {
+        widthInput.value = this.width;
+        heightInput.value = this.height;
+    }
+
+    // console.log('success');
+    // reveal form on success
+    form.style.display='block';
+    //display file name
+    fileName.innerText = file.name;
+    // 
 
 }
 
